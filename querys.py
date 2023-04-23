4964aceca93ae88b9681b0ac175be68f9e4f6e80
Emiliano de la Cruz <Emiliano_r_d_l_c@outlook.com>
2023-04-22 21:00:17 -0600

    query Example

--- querys.py
+++ querys.py
@@ -1,9 +1,17 @@
 from owlready2 import *
-from rdflib import Graph, Namespace
 
-go = get_ontology("mynew.owl").load()
+onto = get_ontology("mynew.owl").load()
+print(onto.base_iri)
 
-print(list(default_world.sparql("""
-           SELECT (COUNT(?x) AS ?nb)
-           { ?x a rdfs:domain . }
-    """)))
+query = """
+PREFIX my_owl: <http://utepdataintegration.org/team3_test#>
+SELECT ?student
+WHERE {
+  ?student rdf:type my_owl:Student .
+}
+"""
+
+results = onto.world.sparql(query)
+for r in results:
+    for i in r:
+        print(i)
