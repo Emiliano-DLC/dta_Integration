5b236cdbfb9f7d8f0bda64bd0782769bbb8fddde
Emiliano de la Cruz <Emiliano_r_d_l_c@outlook.com>
2023-04-22 21:06:05 -0600

    better example of a query

--- querys.py
+++ querys.py
@@ -5,9 +5,12 @@ print(onto.base_iri)
 
 query = """
 PREFIX my_owl: <http://utepdataintegration.org/team3_test#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 SELECT ?student
 WHERE {
   ?student rdf:type my_owl:Student .
+  ?student my_owl:Target ?target .
+  Filter (?target = "Dropout")
 }
 """
 
