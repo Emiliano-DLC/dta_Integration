a6a16cbab1e0e9d7ad9c69fcc0fb61e1cb0922c9
cbmlouhi <cbmlouhi@miners.utep.edu>
2023-04-27 14:17:30 -0600

    Connor Changes

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ app.register_blueprint(query)
 CORS(app)
 
 @app.route("/")
-@app.route("/home")
 def home():
+    return render_template("./home.html")
+
+@app.route("/querySH")
+def qs():
     return render_template("./queryPage.html")
+
+@app.route("/ConQue")
+def connor():
+    return render_template("./ConQuery.html")
